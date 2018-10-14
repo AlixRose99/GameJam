8c1d19b0197076047d043c78cd16e459862e9f4b
Wishes <taeryn@hotmail.ca>
2018-10-14 12:53:56 -0400

    start menu

---
+++ One Button Meltdown/Assets/Scripts/RayCast.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RayCast : MonoBehaviour
+{
+	public Camera camera;
+	RaycastHit hit;
+
+	void Update()
+	{
+
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast(ray, out hit))
+		{
+			Transform objectHit = hit.transform;
+
+			print(objectHit.tag);
+		}
+	}
+}
+
